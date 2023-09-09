858657e83aa405886a63a964bf65fcc485ad7053
Flossy <sfloess@redhat.com>
2023-09-09 19:23:57 -0400

    WIP - Latest

--- src/main/java/org/solenopsis/session/soap/service/SoapEnum.java
+++ src/main/java/org/solenopsis/session/soap/service/SoapEnum.java
@@ -1,14 +1,17 @@
 package org.solenopsis.session.soap.service;
 
+import com.sforce.soap._2006._08.apex.ApexPortType;
+
 /**
  *
  * @author sfloess
  */
 public enum SoapEnum {
-    ENTERPRISE_SERVICE(),
-    PARTNER_SERVICE(),
-    METADATA_SERVICE(),
-    TOOLING_SERVICE();
+    APEX(new Soap(SoapEnum.class.getClassLoader().getResource("wsdl/Session-apex.wsdl")), ApexPortType.class),
+    ENTERPRISE(),
+    PARTNER(),
+    METADATA(),
+    TOOLING();
 
     final Soap soap;
 
